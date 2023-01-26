a2d7a71b25fe5dc3d265197bcef9135906957313
MARKPRO4444 <119742977+MARKPRO4444@users.noreply.github.com>
2023-01-26 20:29:21 +0300

    Task_7

---
+++ Task_7/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Task_7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int num = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            num++;
+            label.Content = num.ToString();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            if(num >= 30)
+            {
+                timer.Stop();
+                MessageBox.Show("Timer - Stop!");
+            }
+        }
+    }
+}
